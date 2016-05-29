927face5329a135c1734a72430e68d1620e8031e
aTasteOfHome <andrewtsai1994@gmail.com>
2016-05-28 22:55:43 -0500

    filled out some html content + restructuring

--- _client/client.js
+++ _client/client.js
@@ -2,11 +2,19 @@
 
 //initialize with dependencies
 var app = angular.module("app",[
+	'ui.router',
 	'MainController'
 ]);
 
-/**
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+app.config(['$stateProvider', '$urlRouterProvider', 
+	function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
-}]);
-*/
+
+        $stateProvider.state('main',{
+        	url: "/",
+        	templateUrl: '/_client/partials/main.html',
+        	controller: 'MainController'
+        });
+	}
+]);
