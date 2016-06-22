956990949550d84f429953a2f7a5c730a91b5076
aTasteOfHome <andrewtsai1994@gmail.com>
2016-06-22 14:24:35 -0500

    added ng-repeat to carousels, made them infinitely scroll

--- _client/controllers/MainController.js
+++ _client/controllers/MainController.js
@@ -29,6 +29,19 @@ app.controller('MainController', ['$scope', '$location', '$anchorScroll',
 				'Adhered to standards used in Electronic Health Records and the Health industry, e.g. HIPAA.'
 			]
 		},{
+			workplace: 'Collegiate Cribs',
+			position:  'Co-creator / Front-end Developer',
+			startDate: 'Aug 2014',
+			endDate: 'May 2015',
+			info: [
+				'Used the  MEAN stack, especially Angular, in conjunction with Polymer, jQuery and other Javascript/HTML/CSS tools to create our website.',
+				'Drafted and finalized technical documentation, including Status Reports, the Statement of Work, and the Feasibility Report.',
+				'Designed mockups of the entire website, including subpages, with an eye towards usability and user-friendly interface design.',
+				'Prepared and performed a presentation of all of our work and research, in conjunction with a walkthrough of our mockups.',
+				'Set up and managed a Git repository structure, and oversaw all merges and code reviews.'
+
+			]
+		},{
 			workplace: 'mGage',
 			position: 'Software Development Intern',
 			startDate: 'May 2014',
@@ -53,7 +66,14 @@ app.controller('MainController', ['$scope', '$location', '$anchorScroll',
 			]
 		}];
 
-		$scope.skillVals=[1,2,3,4,5,6,7,8,9,10];
+		$scope.skillVals= (function() {
+			var arr = [];
+			for(var i=1; i<=10; i++){
+				arr[i-1]=i;
+			}
+			return arr;
+		})();
+
 		$scope.skills=[{
 			name: "Java",
 			skillLevel: "10"
@@ -76,5 +96,84 @@ app.controller('MainController', ['$scope', '$location', '$anchorScroll',
 			name: "SQL",
 			skillLevel: "8"
 		}];
+
+		$scope.likeTypes=[{
+			name: 'Hobbies',
+			likes: [{
+				name: 'Guitar'
+			},{
+				name: 'Piano'
+			},{
+				name: 'Singing'
+			},{
+				name: 'Songwriting'
+			},{
+				name: 'Exercise'
+			},{
+				name: 'Food'
+			}]
+		},{
+			name:"Books",
+			likes:[{
+				name:'Freakonomics'
+			},{
+				name:'Outliers'
+			},{
+				name: '100 Years of Solitude'
+			},{
+				name: 'American Gods'
+			},{
+				name: 'Significant Digits'
+			},{
+				name: 'Harry Potter (series)'
+			},{
+				name: 'Good Omens'
+			},{
+				name: 'Death Trilogy (Discworld)'
+			}]
+		},{
+			name:"Movies",
+			likes:[{
+				name:'12 Angry Men'
+			},{
+				name: 'Exit Through the Gift Shop'
+			},{
+				name: 'Ex Machina'
+			},{
+				name: 'The Dark Knight'
+			},{
+				name: 'Spirited Away'
+			},{
+				name: 'Speedy Scandal'
+			},{
+				name: 'Hot Fuzz'
+			},{
+				name: 'Anchorman'
+			}]
+
+		},{
+			name: 'Music',
+			likes: [{
+				name: 'Ratatat'
+			},{
+				name: 'The Strokes'
+			},{
+				name: 'Alabama Shakes'
+			},{
+				name: 'Shakey Graves'
+			},{
+				name: 'Julien Baker'
+			},{
+				name: 'Death Cab for Cutie'
+			},{
+				name: 'The Black Keys'
+			},{
+				name: 'Bombay Bicycle Club'
+			},{
+				name: 'Tycho'
+			},{
+				name: 'The Lumineers'
+			}]
+		}]
 	}]
 );
