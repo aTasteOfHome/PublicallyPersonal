19dc465126dbe2bc027ca38f4743d1dfa4ad23c4
aTasteOfHome <andrewtsai1994@gmail.com>
2016-07-01 15:45:48 -0500

    updated readme, experiments with the sticky directive

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # PublicallyPersonal
 Personal website that will hold work-related information and achievements.
+
+To run: be sure you have these installed:
+
+Node.js
+npm
+Git
+bower
+
+Then:
+1. Check to see you have these programs installed:
+	Node.js
+	npm
+	Git
+2. git clone https://github.com/aTasteOfHome/PublicallyPersonal.git
+3. Go to PublicallyPersonal directory
+4. npm install
+5. bower install
+6. npm start
--- _client/directives/mySticky.js
+++ _client/directives/mySticky.js
@@ -6,6 +6,8 @@ mod.directive("sticky", function($window) {
     	var elementPos= element[0].getBoundingClientRect();
         var initialTop=elementPos.top;
         angular.element($window).bind("scroll", function() { 
+        	// angular.element(element[0].querySelectorAll('td')).css('border-color','red');
+        	// angular.element(element[0].querySelectorAll('td')).css('border-width', this.pageYOffset+'px');
 			scope.navbarStuck = this.pageYOffset >= initialTop;
             scope.$apply();
         });
